8129762e371e1dcbb86d25257625ff257d7a761f
kovand42 <kovand42@gmail.com>
2019-02-15 13:47:08 +0100

    zonder gitignore

---
+++ dependencies1/src/test/java/be/vdab/services/LandServiceTest.java
@@ -0,0 +1,24 @@
+package be.vdab.services;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import java.math.BigDecimal;
+import be.vdab.repositories.LandRepository;
+import be.vdab.repositories.LandRepositoryStub;
+
+public class LandServiceTest {
+	private LandRepository landRepository;
+	private LandService landService;
+	@Before
+	public void before() {
+		landRepository = new LandRepositoryStub();
+		landService = new LandService(landRepository);
+	}
+	@Test
+	public void findVerhoudingOppervlakteLandTovOppervlakteAlleLanden() {
+		assertEquals(0, BigDecimal.valueOf(0.25).compareTo(
+				landService.findVerhoudingOppervlakteLandTovOppervlakteAlleLanden("B")));
+	}
+
+}
